adbba7ab261d2071964e1620e7907532bb57bc91
gorgeous-karma-7335 <graj08@gmail.com>
2019-05-14 19:58:43 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 holiday_hash.each do |seasons|
-  puts "#{seasons.keys.to_s}"
+  puts "#{seasons}:"
 end
 end
 
