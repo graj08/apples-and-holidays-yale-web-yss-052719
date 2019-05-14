a6a0746cde5b1d4a7c35e8f2a66d5d14922b503b
gorgeous-karma-7335 <graj08@gmail.com>
2019-05-14 20:25:04 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -80,22 +80,3 @@ holiday_hash.each do |season, holiday|
   end
 end.flatten.compact
 end
-
-holiday_supplies =
-{
-   :winter => {
-     :christmas => ["Lights", "Wreath"],
-     :new_years => ["Party Hats"]
-   },
-   :summer => {
-     :fourth_of_july => ["Fireworks", "BBQ"]
-   },
-   :fall => {
-     :thanksgiving => ["Turkey"]
-   },
-   :spring => {
-     :memorial_day => ["BBQ"]
-   }
- }
-
- all_supplies_in_holidays(holiday_supplies)
